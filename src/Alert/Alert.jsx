0899ca6a00294323bb6659b3c1e9b5675695fecb
Yunus <seytnazarov.yunus@gmail.com>
2022-06-06 23:51:07 +0400

    Alert

---
+++ src/Alert/Alert.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Alert extends React.Component {
+  render() {
+    return(
+      <div className="alert alert-warning"  role="alert">{this.props.text}</div>   
+    )
+  }
+}
+
+export default Alert;
